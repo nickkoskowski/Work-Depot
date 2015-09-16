1915203025b973c9e0d3a8fb33fcfb8f61cdf35f
nickkoskowski <nick.k@adboomgrp.com>
2015-09-16 10:49:26 -0700

    Refactored header.php and footer.php
    Called to index.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php wp_title(); ?></title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h1>Work Depot</h1>
+				</div>
+				<div class="col-md-8 text-right">
+				</div>
+			</div>
+		</div>
+	</header>
